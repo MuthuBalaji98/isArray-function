71d9cf7c2f18910f131b20eaf7cc8162f199b391
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:02:36 +0530

    isArray

---
+++ main.js
@@ -0,0 +1,7 @@
+let text=["Hello", "World"];
+console.log("original array :", text);
+console.log("length of the array is "+text.length);
+console.log("method : Array.isArray()");
+let result = Array.isArray(text);
+console.log("change in array : ", result);
+console.log("return: length of array after Array.isArray() is "+result.length);
